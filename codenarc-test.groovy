a5a3cbe8165a1fca9275d0f9b50671e91fd3bcfa
brentdemark <brent.demark@identifix.com>
2017-12-29 09:58:14 -0600

    Removing deprecated rules

--- codenarc-test.groovy
+++ codenarc-test.groovy
@@ -207,7 +207,7 @@ ruleset {
     GrailsMassAssignment
     GrailsPublicControllerMethod
     GrailsServletContextReference
-    GrailsSessionReference   // DEPRECATED
+//    GrailsSessionReference   // DEPRECATED
     GrailsStatelessService
 
     // rulesets/groovyism.xml
@@ -331,7 +331,7 @@ ruleset {
     //SerializableClassMustDefineSerialVersionUID
 
     // rulesets/size.xml
-    AbcComplexity   // DEPRECATED: Use the AbcMetric rule instead. Requires the GMetrics jar
+//    AbcComplexity   // DEPRECATED: Use the AbcMetric rule instead. Requires the GMetrics jar
     AbcMetric   // Requires the GMetrics jar
     ClassSize
     CrapMetric   // Requires the GMetrics jar and a Cobertura coverage file
