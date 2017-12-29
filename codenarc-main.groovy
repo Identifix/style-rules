3b5fb9f8b8508364179022be8c8b9589c748a725
brentdemark <brent.demark@identifix.com>
2017-12-29 09:55:40 -0600

    Remove deprecated rules

--- codenarc-main.groovy
+++ codenarc-main.groovy
@@ -331,7 +331,7 @@ ruleset {
     //SerializableClassMustDefineSerialVersionUID
 
     // rulesets/size.xml
-    AbcComplexity   // DEPRECATED: Use the AbcMetric rule instead. Requires the GMetrics jar
+//    AbcComplexity   // DEPRECATED: Use the AbcMetric rule instead. Requires the GMetrics jar
     AbcMetric   // Requires the GMetrics jar
     ClassSize
     CrapMetric   // Requires the GMetrics jar and a Cobertura coverage file
